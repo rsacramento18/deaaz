4e2c556e43d955fe1d69202e70fbb308502f329d
Ricardo Sacramento <ricardo.sacramento@outlook.com>
2019-05-03 20:14:03 +0100

    Started backend development

--- deaaz-app/src/app/blog/blog.component.ts
+++ deaaz-app/src/app/blog/blog.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
   private letters: String[] = [];
+  private tags: String[] = [];
 
   constructor() { }
 
@@ -18,26 +19,18 @@ export class BlogComponent implements OnInit {
     this.letters.push("E");
     this.letters.push("F");
     this.letters.push("G");
-    this.letters.push("H");
-    this.letters.push("I");
-    this.letters.push("J");
-    this.letters.push("K");
-    this.letters.push("L");
-    this.letters.push("M");
-    this.letters.push("N");
-    this.letters.push("O");
-    this.letters.push("P");
-    this.letters.push("Q");
-    this.letters.push("R");
-    this.letters.push("S");
-    this.letters.push("T");
-    this.letters.push("U");
-    this.letters.push("V");
-    this.letters.push("W");
-    this.letters.push("X");
-    this.letters.push("Y");
-    this.letters.push("Z");
     console.log(this.letters);
+
+    this.tags.push("Criativa");
+    this.tags.push("Poesia");
+    this.tags.push("Romance");
+    this.tags.push("Crítica");
+    this.tags.push("Crónica");
+    this.tags.push("Novela");
+    this.tags.push("Fábula");
+    this.tags.push("Parábula");
+    this.tags.push("Lenda");
+    this.tags.push("Anedota");
   }
 
 }
